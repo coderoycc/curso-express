e2beb0e57cea8de2683271c796b9711e1f48ed77
coderoycc <rcchambi4@umsa.bo>
2023-02-20 13:05:12 -0400

    HTML metodos

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # CURSO DE EXPRESS
 Express es un framework para el Backend que se ejecuta con NODEJS. 
 
+
+## Verbos - Methodos HTTP
+- GET: Peticion simple
+- POST: Crear algun datos (BODY)
+- PUT: Actualizar datos (BODY)
+- DELETE: Eliminar algun dato (BODY)
+Todas las peticiones se procesan de la misma manera, usando la aplicacion con el metodo
+```js
+app.get('ruta/endpoint', (req,res)=>{})
+app.post(enpoint, function)
+app.delete(endpoint, function)
+app.put(endpoint, function)
+```
+
+## Recepción de datos 
+- Para recibir texto y que express lo pueda interpretar se debe usar `app.use(express.text())`
+- Para recibir json y que express lo pueda interpretar se debe usar `app.use(express.json())`
+- Para recibit datos de formularios simples se usa `app.use(express.urlencoded({extended: false}))`
--- index.js
+++ index.js
@@ -1,13 +1,98 @@
-// Importamos http y filesystem
-const http = require('http')
-const fs = require('fs')
+// Construcción simple sin EXPRESS
 
-// Declaramos el servidor
-const server = http.createServer((req, res)=>{
-  // Creamos un recurso de lectura de archivo html
-  const read = fs.createReadStream('./static/index.html')
-  
-  // Escribimos el archivo html mediante un canal (pipe) hacia la respuesta (res) 
-  read.pipe(res)
+
+// // Importamos http y filesystem
+// const http = require('http')
+// const fs = require('fs')
+
+// // Declaramos el servidor
+// const server = http.createServer((req, res)=>{
+//   // Creamos un recurso de lectura de archivo html
+//   const read = fs.createReadStream('./static/index.html')
+
+//   // Escribimos el archivo html mediante un canal (pipe) hacia la respuesta (res) 
+//   read.pipe(res)
+// })
+
+// server.listen(3000, ()=>{
+//   console.log("Servidor en el puerto 3000")
+// })
+
+
+
+// EXPRESS
+
+const { json } = require('express')
+const express = require('express')
+
+// Declaramos la aplicacion 
+const app = express()
+
+// recepción de texto con express
+app.use(express.text())
+// Recepción de json con express
+app.use(express.json())
+// Recepción de formmularios con express
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => { // Cuando visiten la raiz
+  // Ruta desde el sistema inicial hasta este proyecto
+  console.log(__dirname)
+
+  // Enviamos el archivo html mendiate sendFile(ruta, {ruta-raiz base})
+  res.sendFile('./static/index.html', { root: __dirname })
 })
+
+app.get('/about', (req, res) => {
+  res.send('<h1>Esta página describe más de nosotros</h1>')
+})
+
+app.get('/clima', (req, res) => {
+  res.send('Enviamos datos del clima')
+})
+
+//Enviamos una imagen a la peticion GET
+app.get('/imagen', (req, res) => {
+  res.sendFile('./static/cora.jpg', { root: __dirname })
+})
+
+//Enviar datos json
+app.get('/json', (req, res) => {
+  res.json({ hola: 'Putito', marica: 'Puto' })
+})
+
+
+// PETICIONES con verbos HTTP
+app.post('/texto', (req, res) => { // recibimos texto en el body
+  console.log(req.body)
+  res.status(200).send('Llego tu comunicado')
+})
+
+app.post('/json', (req, res) => { // Recibimos json en el body
+  console.log(req.body)
+  res.status(200).send('Json recibido')
+})
+
+app.post('/form', (req, res) => {
+  console.log(req.body)
+  res.status(200).send('Recibido')
+})
+
+
+// Esta página se cargará despues de consultar las anteriores
+app.use((req, res) => {
+  // Respondemos con un status 404 de no encontrado
+  res.status(404).send('No se encontro la página que busca')
+})
+app.listen(3000, () => {
+  console.log("Aplicacion con EXPRESS corriendo en el puerto 3000")
+})
+
+
+
+
+
+
+
 
