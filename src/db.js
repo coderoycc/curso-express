0c8c21703cbe251898c587c1e75f6a5a8a08ab07
coderoycc <rcchambi4@umsa.bo>
2023-03-07 15:35:54 -0400

    Uso de dotenv para proteger contraseñas

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
-
+require('dotenv').config() 
 async function connectDB(){
   const conn = await mysql.createConnection({
-    host:'us-east.connect.psdb.cloud',
-    user:'ueyc0as3p44u5q7q57ru',
-    password:'pscale_pw_qTHEkv2nxBDCPsuL7rRpwPRss6fg5W2qOWHWJoaQ4iG',
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASS,
     database:'betto',
     ssl: {
       rejectUnauthorized: false
@@ -13,5 +13,4 @@ async function connectDB(){
   const res = conn.query('SELECT 1 + 1 AS Result')
   console.log(res)
 }
-
 module.exports = connectDB
